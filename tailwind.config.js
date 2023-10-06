3edfd7e5bdbf4f7a5b649ccf4aa94e214ed0df53
Domnyus <domnyus@gmail.com>
2023-10-06 19:42:07 -0300

    2023-10-06 portfolio v1

---
+++ tailwind.config.js
@@ -0,0 +1,9 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/*.{css,js,html}","./public/*.{css,js,html}"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
